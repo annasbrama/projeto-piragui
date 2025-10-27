21c5512bd579a2d8b3763540719d5f9aac6801e1
Anna <acesbrama@gmail.com>
2025-10-27 16:00:32 -0300

    feat: rota inserida no link no pages/campaigns-screen

--- src/app/pages/campaigns-screen/campaigns-screen.component.ts
+++ src/app/pages/campaigns-screen/campaigns-screen.component.ts
@@ -1,4 +1,6 @@
+import { ViewportScroller } from '@angular/common';
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-campaigns-screen',
@@ -7,4 +9,28 @@ import { Component } from '@angular/core';
 })
 export class CampaignsScreenComponent {
 
+  campaignsLink = [
+    { route: "/", fragment: "como-ajudar" }
+  ]
+
+  constructor(
+    public router: Router,
+    private viewportScroller: ViewportScroller
+  ) { }
+
+  navigateTo(route: string, fragment?: string): void {
+    if (fragment) {
+      this.router.navigate([route], { fragment }).then(() => {
+        setTimeout(() => {
+          this.viewportScroller.scrollToAnchor(fragment)
+        }, 100)
+      })
+    } else {
+      this.router.navigate([route])
+    }
+  }
+
+  navigateToMobile(route: string, fragment?: string): void {
+    this.navigateTo(route, fragment);
+  }
 }
