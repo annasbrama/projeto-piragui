127ead9869d3f8f51d6763014bf54f1b3b508c1a
Anna <acesbrama@gmail.com>
2025-11-03 22:35:10 -0300

    feat: link fragmentado adicionado na page about-us-screen

--- src/app/pages/about-us-screen/about-us-screen.component.ts
+++ src/app/pages/about-us-screen/about-us-screen.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ViewportScroller } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about-us-screen',
@@ -6,5 +8,28 @@ import { Component } from '@angular/core';
   styleUrls: ['./about-us-screen.component.css']
 })
 export class AboutUsScreenComponent {
+  aboutLink = [
+    { route: "/", fragment: "como-ajudar" }
+  ]
 
+  constructor(
+    public router: Router,
+    private viewportScroller: ViewportScroller
+  ) { }
+
+  navigateTo(route: string, fragment?: string): void {
+    if (fragment) {
+      this.router.navigate([route], { fragment }).then(() => {
+        setTimeout(() => {
+          this.viewportScroller.scrollToAnchor(fragment)
+        }, 100)
+      })
+    } else {
+      this.router.navigate([route])
+    }
+  }
+
+  navigateToMobile(route: string, fragment?: string): void {
+    this.navigateTo(route, fragment);
+  }
 }
