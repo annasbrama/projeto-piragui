73f1019cef28546059f85641f3da72a3cfd6dccb
Anna <acesbrama@gmail.com>
2025-11-09 07:12:23 -0300

    feat: copia e cola automático na chave pix na page home-screen

--- src/app/pages/home-screen/home-screen.component.ts
+++ src/app/pages/home-screen/home-screen.component.ts
@@ -1,16 +1,33 @@
-import { Component, OnInit } from '@angular/core';
-import { Title } from '@angular/platform-browser';
+import { Component, OnInit } from "@angular/core"
+import { Title } from "@angular/platform-browser"
 
 @Component({
-  selector: 'app-home-screen',
-  templateUrl: './home-screen.component.html',
-  styleUrls: ['./home-screen.component.css']
+  selector: "app-home-screen",
+  templateUrl: "./home-screen.component.html",
+  styleUrls: ["./home-screen.component.css"],
 })
 export class HomeScreenComponent implements OnInit {
+  pixKey = "projetopiragui@gmail.com"
+  showCopiedNotification = false
 
   constructor(private titleService: Title) {}
 
   ngOnInit(): void {
-    this.titleService.setTitle('Início • Projeto Piragui');
+    this.titleService.setTitle("Início • Projeto Piragui")
+  }
+
+  copyPixKey(): void {
+    navigator.clipboard
+      .writeText(this.pixKey)
+      .then(() => {
+        this.showCopiedNotification = true
+
+        setTimeout(() => {
+          this.showCopiedNotification = false
+        }, 3000)
+      })
+      .catch((err) => {
+        console.error("Erro ao copiar:", err)
+      })
   }
 }
